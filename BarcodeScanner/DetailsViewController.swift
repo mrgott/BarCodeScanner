449b79ba533fe54ff290fdb8f415538fe60c71a8
Mikheil Gotiashvili <mrGott@Mikheils-Mac-mini.local>
2017-07-30 13:44:40 +0400

    Initial Commit

---
+++ BarcodeScanner/DetailsViewController.swift
@@ -0,0 +1,74 @@
+//
+//  DetailsViewController.swift
+//  BarcodeScanner
+//
+//  Created by Mikheil Gotiashvili on 7/29/17.
+//  Copyright © 2017 Mikheil Gotiashvili. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    var scannedCode:String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        print(scannedCode!)
+        
+        
+        // Setup label and button layout
+        view.addSubview(codeLabel)
+        codeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100).isActive = true
+        codeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        codeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        codeLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        if let scannedCode = scannedCode {
+            codeLabel.text = scannedCode
+        }
+        
+        view.addSubview(scanButton)
+        scanButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive = true
+        scanButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        scanButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        scanButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    let codeLabel:UILabel = {
+        let codeLabel = UILabel()
+        codeLabel.textAlignment = .center
+        codeLabel.backgroundColor = .white
+        codeLabel.translatesAutoresizingMaskIntoConstraints = false
+        return codeLabel
+    }()
+    
+    lazy var scanButton:UIButton = {
+        let scanButton = UIButton(type: .system)
+        scanButton.setTitle("Scan", for: .normal)
+        scanButton.setTitleColor(.white, for: .normal)
+        scanButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        scanButton.backgroundColor = .orange
+        scanButton.layer.cornerRadius = 25
+        scanButton.addTarget(self, action: #selector(displayScannerViewController), for: .touchUpInside)
+        scanButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scanButton
+    }()
+    
+    @objc func displayScannerViewController() {
+        print("123")
+        let scanViewController = ScannerViewController()
+        //navigationController?.pushViewController(scanViewController, animated: true)
+        //navigationController?.present(scanViewController, animated: true, completion: nil)
+        present(scanViewController, animated: true, completion: nil)
+    }
+
+}
---
+++ BarcodeScanner/ScannerViewController.swift
@@ -0,0 +1,131 @@
+//
+//  ViewController.swift
+//  BarcodeScanner
+//
+//  Created by Mikheil Gotiashvili on 7/14/17.
+//  Copyright © 2017 Mikheil Gotiashvili. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AudioToolbox
+
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    var captureDevice:AVCaptureDevice?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var captureSession:AVCaptureSession?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "Scanner"
+        view.backgroundColor = .white
+        
+        captureDevice = AVCaptureDevice.default(for: .video)
+        // Check if captureDevice returns a value and unwrap it
+        if let captureDevice = captureDevice {
+        
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice)
+                
+                captureSession = AVCaptureSession()
+                guard let captureSession = captureSession else { return }
+                captureSession.addInput(input)
+                
+                let captureMetadataOutput = AVCaptureMetadataOutput()
+                captureSession.addOutput(captureMetadataOutput)
+                
+                captureMetadataOutput.setMetadataObjectsDelegate(self, queue: .main)
+                captureMetadataOutput.metadataObjectTypes = [.code128, .qr, .ean13,  .ean8, .code39] //AVMetadataObject.ObjectType
+                
+                captureSession.startRunning()
+                
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                videoPreviewLayer?.videoGravity = .resizeAspectFill
+                videoPreviewLayer?.frame = view.layer.bounds
+                view.layer.addSublayer(videoPreviewLayer!)
+                
+            } catch {
+                print("Error Device Input")
+            }
+            
+        }
+        
+        view.addSubview(codeLabel)
+        codeLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        codeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        codeLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        codeLabel.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    let codeLabel:UILabel = {
+        let codeLabel = UILabel()
+        codeLabel.backgroundColor = .white
+        codeLabel.translatesAutoresizingMaskIntoConstraints = false
+        return codeLabel
+    }()
+    
+    let codeFrame:UIView = {
+        let codeFrame = UIView()
+        codeFrame.layer.borderColor = UIColor.green.cgColor
+        codeFrame.layer.borderWidth = 2
+        codeFrame.frame = CGRect.zero
+        codeFrame.translatesAutoresizingMaskIntoConstraints = false
+        return codeFrame
+    }()
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects.count == 0 {
+            //print("No Input Detected")
+            codeFrame.frame = CGRect.zero
+            codeLabel.text = "No Data"
+            return
+        }
+        
+        let metadataObject = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        guard let stringCodeValue = metadataObject.stringValue else { return }
+        
+        view.addSubview(codeFrame)
+        
+        guard let barcodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObject) else { return }
+        codeFrame.frame = barcodeObject.bounds
+        codeLabel.text = stringCodeValue
+        
+        // Play system sound with custom mp3 file
+        if let customSoundUrl = Bundle.main.url(forResource: "beep-07", withExtension: "mp3") {
+            var customSoundId: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(customSoundUrl as CFURL, &customSoundId)
+            //let systemSoundId: SystemSoundID = 1016  // to play apple's built in sound, no need for upper 3 lines
+            
+            AudioServicesAddSystemSoundCompletion(customSoundId, nil, nil, { (customSoundId, _) -> Void in
+                AudioServicesDisposeSystemSoundID(customSoundId)
+            }, nil)
+            
+            AudioServicesPlaySystemSound(customSoundId)
+        }
+        
+        
+        // Stop capturing and hence stop executing metadataOutput function over and over again
+        captureSession?.stopRunning()
+        
+        // Call the function which performs navigation and pass the code string value we just detected
+        displayDetailsViewController(scannedCode: stringCodeValue)
+        
+    }
+    
+    func displayDetailsViewController(scannedCode: String) {
+        let detailsViewController = DetailsViewController()
+        detailsViewController.scannedCode = scannedCode
+        //navigationController?.pushViewController(detailsViewController, animated: true)
+        present(detailsViewController, animated: true, completion: nil)
+    }
+
+}
